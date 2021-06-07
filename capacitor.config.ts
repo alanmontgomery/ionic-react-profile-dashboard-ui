5b846d8e7364e71c7b4b16cf0079c684933781e3
Alan Montgomery <44590018+alanmontgomery@users.noreply.github.com>
2021-06-07 23:18:28 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'ionic-react-profile-ui',
+  appName: 'ionic-react-profile-dashboard-ui',
   webDir: 'build',
   bundledWebRuntime: false
 };
